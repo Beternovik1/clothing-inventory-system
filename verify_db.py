224569d54538d06a84ab0ecbeffc04ed05207ad8
Edgar <edgaralfarohernandez15@gmail.com>
2026-01-11 10:46:31 -0800

    feature: Add finishing the backend connection

--- verify_db.py
+++ verify_db.py
@@ -1,42 +1,48 @@
 import mysql.connector
+# We import the get_db_function object itself but we don't 
+# add () at the end because we're NOT running it yet
 from db_config import get_db_config
 
-def verify_table_structure(table_name):
-    config = get_db_config()
+def verify_db(table_name):
     connection = None
     cursor = None
-    
+
     try:
-        # 1. Connect
+        config = get_db_config()
         connection = mysql.connector.connect(**config)
         cursor = connection.cursor()
-        
-        # 2. Inspect
-        print(f"\n--- INSPECTING: {table_name} ---")
-        cursor.execute(f"DESCRIBE {table_name}")
-        
-        # 3. Print Header
-        print(f"{'Field':<15} {'Type':<15} {'Null':<5} {'Key':<5}")
+
+        print("Tables in database")
+        cursor.execute("Show tables")
+
+
+        for table in cursor:
+            print(table[0])
+
+        # Describe is an sql command that shows the structure of the table (only the column names,
+        # data types, and if a column can be empty/null)
+        cursor.execute(f'Describe {table_name}')
+        print(f'{'Field':<15} {'Type':<15} {'Null':<15} {'Key':<15}')
         print("-" * 45)
 
-        # 4. Print Rows
         for row in cursor:
-            print(f"{row[0]:<15} {row[1]:<15} {row[2]:<5} {row[3]:<5}")
-
+            # row format: (Field, Type, Null, Key, Default, Extra)
+            print(f'{row[0]:<15} {row[1]:<15} {row[2]:<15} {row[3]:<15}')
     except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        
+        print(f'Error: {err}')
     finally:
-        # 5. The Safety Net
-        # We check if 'cursor' and 'connection' exist before closing
-        # to avoid errors if the connection failed at the very start.
         if cursor:
             cursor.close()
-            print("\nCursor closed.")
-        if connection:
+            print("Cursor closed")
+        if connection and connection.is_connected():
             connection.close()
-            print("Connection closed.")
+            print("Connection closed")
 
-# Main execution
 if __name__ == "__main__":
-    verify_table_structure('products')
+    verify_db('products')
+    # verify_db('inventory_movements')
+
+    
+
+
+
